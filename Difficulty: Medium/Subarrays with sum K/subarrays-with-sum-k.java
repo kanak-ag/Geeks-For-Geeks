4b9e33e59e7e8b6db0df7a4c907f378e62314c78
Kanak Agrawal <133563970+kanak-ag@users.noreply.github.com>
2025-01-02 23:42:49 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Subarrays with sum K/subarrays-with-sum-k.java
@@ -0,0 +1,60 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String args[]) throws IOException {
+        // taking input using class Scanner
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
+
+        while (t-- > 0) {
+            // taking total number of elements
+            int k = Integer.parseInt(br.readLine());
+            String line = br.readLine();
+            String[] tokens = line.split(" ");
+
+            // Create an ArrayList to store the integers
+            ArrayList<Integer> array = new ArrayList<>();
+
+            // Parse the tokens into integers and add to the array
+            for (String token : tokens) {
+                array.add(Integer.parseInt(token));
+            }
+
+            int[] arr = new int[array.size()];
+            int idx = 0;
+            for (int i : array) arr[idx++] = i;
+            int res = new Solution().countSubarrays(arr, k);
+
+            System.out.print(res);
+            System.out.println();
+            System.out.println("~");
+        }
+    }
+}
+// } Driver Code Ends
+
+
+// User function Template for Java
+class Solution {
+    public int countSubarrays(int arr[], int k) {
+        HashMap<Integer,Integer> hm=new HashMap<>();
+        hm.put(0,1);
+        int ans=0;
+        int prefix=0;
+        for(int i:arr){
+            prefix+=i;
+            if(hm.containsKey(prefix-k)){
+                ans+=hm.get(prefix-k);
+                
+            }
+            hm.put(prefix, hm.getOrDefault(prefix, 0) + 1);
+            
+        }
+        return ans;
+    }
+}
